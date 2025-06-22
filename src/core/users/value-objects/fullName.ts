5b9072f8a63bf8b2769995c47a22ab3ea69957f6
Luan Campos <luancamposdeveloper@gmail.com>
2025-06-22 17:29:44 -0300

    chore: create fullname value objects

---
+++ src/core/users/value-objects/fullName.ts
@@ -0,0 +1,34 @@
+export class FullName {
+  private readonly _fullName: string;
+
+  get value(): string {
+    return this._fullName;
+  }
+
+  static create(name: string): FullName {
+    if (!FullName.isValid(name)) {
+      throw new Error('Invalid name. please enter valid name.');
+    }
+
+    return new FullName(name.trim());
+  }
+
+  public getValue(): string {
+    return this._fullName;
+  }
+
+  private static isValid(name: string): boolean {
+    if (!name) return false;
+
+    const trimmed: string = name.trim();
+    const parts = trimmed.split(/\s+/);
+
+    if (parts.length < 2) return false;
+
+    return parts.every((part) => /^[A-Za-zÀ-ÿ]{2,}$/.test(part));
+  }
+
+  private constructor(fullName: string) {
+    this._fullName = fullName;
+  }
+}
