01366e5ef19f3c20cd89482efe3614b8feec372d
Luan Campos <luancamposdeveloper@gmail.com>
2025-06-23 12:03:16 -0300

    chore: create port user-repository

--- src/core/users/repositories/user-repository.ts
+++ src/core/users/repositories/user-repository.ts
@@ -1,5 +1,7 @@
 import { User } from '../entities/user';
+import { Email } from '../value-objects/email.vo';
 
 export abstract class UserRepository {
   abstract create(user: User): Promise<void>;
+  abstract findByEmail(email: Email): Promise<User | null>;
 }
