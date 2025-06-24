72e9d2bcb47fc1974cb76d729380b868fe88487e
Luan Campos <luancamposdeveloper@gmail.com>
2025-06-23 23:10:40 -0300

    chore: add new features

--- src/core/users/repositories/user-repository.ts
+++ src/core/users/repositories/user-repository.ts
@@ -1,7 +1,10 @@
+import { UUID } from 'node:crypto';
 import { User } from '../entities/user';
 import { Email } from '../value-objects/email.vo';
 
 export abstract class UserRepository {
   abstract create(user: User): Promise<void>;
   abstract findByEmail(email: Email): Promise<User | null>;
+  abstract findById(id: UUID): Promise<User | null>;
+  abstract save(user: User): Promise<void>;
 }
--- test/in-memory-user-repository.ts
+++ test/in-memory-user-repository.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/require-await */
+import { UUID } from 'crypto';
 import { User } from '../src/core/users/entities/user';
 import { UserRepository } from '../src/core/users/repositories/user-repository';
 import { Email } from '../src/core/users/value-objects/email.vo';
@@ -13,7 +14,20 @@ export class InMemoryRepository implements UserRepository {
     return user ?? null;
   }
 
+  async findById(id: UUID): Promise<User | null> {
+    const user = this.users.find((u) => u.id === id);
+
+    return user ?? null;
+  }
+
   async create(user: User): Promise<void> {
     this.users.push(user);
+  }
+
+  async save(user: User): Promise<void> {
+    const index = this.users.findIndex((u) => u.id === user.id);
+    if (index >= 0) {
+      this.users[index] = user;
+    }
   }
 }
